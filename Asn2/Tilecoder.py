d281110fa058ed23f8a34280e1c0c82ea8920eba
vanbelle <vanbelle@ualberta.ca>
2015-11-20 21:33:01 -0700

    Tilecode works -- hopefully

--- Asn2/Tilecoder.py
+++ Asn2/Tilecoder.py
@@ -1,12 +1,24 @@
 numTilings = 8
-    
+offset = 0.6/numTilings
+
+#tile code takes 2 numbers (x,y) and translates them into the tile number 
+#in which the numbers land after each tiling.
 def tilecode(x,y,tileIndices):
-    # write your tilecoder here (5 lines or so)
-    
+    #finding the tile corresponding to the vectors is repeated 8 times  
+    for i in range(0,numTilings):
+        # row/column are determined by adding i*offset to the tiling index to repersent the 
+        # matrix offset and then dividing by 0.6 (the width per tile) to determine the row or column
+        column = (x + i*offset) // 0.6
+        row = (y + i*offset) // 0.6 
+        # the tile number is found using a base number i*121 to show which tiling we are on, and then 
+        # adding column*10 and row to bring us to the tile within a particular tiling 
+        tile = (i*121) + (11*column) + row
+        tileIndices[i] = int(tile)
+    return tileIndices    
     
 def printTileCoderIndices(x,y):
     tileIndices = [-1]*numTilings
-    tilecode(x,y,tileIndices)
+    tileIndices = tilecode(x,y,tileIndices)
     print 'Tile indices for input (',x,',',y,') are : ', tileIndices
 
 printTileCoderIndices(0.1,0.1)
