4a45d7e98a76b087fea1e7023921dbfa436567df
chasemc67 <chasemc67@gmail.com>
2015-11-23 13:57:43 -0700

    added submission folder

---
+++ Asn2/Submission/Part 1/Tilecoder.py
@@ -0,0 +1,51 @@
+## HOW IT WORKS ############################################################################
+#   takes 2 numbers, x and y, and translates them 
+#   into their corresponding tile number in each tiling. 
+#   Because there are 8 tilings, it returns 8 values (the 
+#   tile corresponding to x,y in each tiling)
+#
+#   to determine these tiles:
+#   for each tile coding, the the tile is 
+#   (x and y + the current offset) / 0.6 and then rounded down 
+#   to whole numbers
+#
+#   what this does, is puts very similar input together on some 
+#   tiles but not all tiles. for example see samples 2, and 4 in this output
+#
+#   Tile indices for input ( 0.1 , 0.1 ) are :  [0, 121, 242, 363, 484, 605, 726, 859]
+#   Tile indices for input ( 4.0 , 2.0 ) are :  [69, 190, 311, 443, 564, 685, 807, 928]
+#   Tile indices for input ( 5.99 , 5.99 ) are :  [108, 241, 362, 483, 604, 725, 846, 967]
+#   Tile indices for input ( 4.0 , 2.1 ) are :  [69, 190, 311, 443, 565, 686, 807, 928]
+#
+#   Modified by Chase McCarty and Sarah Van Belleghem in 2015
+############################################################################################
+
+
+numTilings = 8
+offset = 0.6/numTilings
+
+#tile code takes 2 numbers (x,y) and translates them into the tile number 
+#in which the numbers land after each tiling.
+def tilecode(x,y,tileIndices):
+    #finding the tile corresponding to the vectors is repeated 8 times  
+    for i in range(0,numTilings):
+        # row/column are determined by adding i*offset to the tiling index to repersent the 
+        # matrix offset and then dividing by 0.6 (the width per tile) to determine the row or column
+        column = (x + i*offset) // 0.6
+        row = (y + i*offset) // 0.6 
+        # the tile number is found using a base number i*121 to show which tiling we are on, and then 
+        # adding column*10 and row to bring us to the tile within a particular tiling 
+        tile = (i*121) + (11*column) + row
+        tileIndices[i] = int(tile)
+    return tileIndices    
+    
+def printTileCoderIndices(x,y):
+    tileIndices = [-1]*numTilings
+    tileIndices = tilecode(x,y,tileIndices)
+    print 'Tile indices for input (',x,',',y,') are : ', tileIndices
+
+#printTileCoderIndices(0.1,0.1)
+#printTileCoderIndices(4.0,2.0)
+#printTileCoderIndices(5.99,5.99)
+#printTileCoderIndices(4.0,2.1)
+    
---
+++ Asn2/Submission/Part 2/SuperLearn.py
@@ -0,0 +1,101 @@
+## HOW IT WORKS ############################################################################
+## Uses tilecoder to learn.
+## See f(x,y) and learn(x,y,target) for the meat of what's going on.
+##
+## Modified by Chase McCarty and Sarah Van Belleghem in 2015
+############################################################################################
+
+from pylab import zeros, sin, cos, normal, random
+from Tilecoder import numTilings, tilecode
+
+# initialize weights appropriately here
+theta = [0] * 968
+# initialize step size parameter appropriately here
+alpha = (0.1/numTilings)
+# initialize your global list of tile indices here
+phi = [0] * 968
+    
+# Returns what AI's estimate f(x,y) = z 
+# Finds the "numTiles" tiles corresponding to the given x,y
+# Each tile makes up 1/"numTiles" the value of the estimated z. 
+# Sum up the 8 theta values, and return the estimated z
+def f(x,y):
+    # write your linear function approximator here (5 lines or so)
+    total = 0
+    vectorLength = len(theta) # corresponds to n in the algorithm
+    featureVectorArray = tilecode(x,y,[-1]*numTilings)
+    for i in range(vectorLength):
+        if i in featureVectorArray: 
+            total += theta[i] 
+    return total
+   
+# is given a sample x,y and true z for f(x,y) = x.
+# Grabs the "numTiles" tiles corresponding to the given x and y.
+# moves each theta corresponding to a tile alpha/"numTilings" closer to the
+# true value it should be
+def learn(x,y,target):
+    # write your gradient descent learning algorithm here (3 lines or so)
+    currentFXY = f(x,y)
+    featureVectorArray = tilecode(x,y,[-1]*numTilings)
+    for i in range(len(theta)):
+        if i in featureVectorArray:
+            theta[i] = theta[i] + alpha*(target - currentFXY)
+
+
+def test1():
+    for x,y,target in \
+         [ (0.1, 0.1, 3.0), \
+           (4.0, 2.0, -1.0), \
+           (5.99, 5.99, 2.0), \
+           (4.0, 2.1, -1.0) ]:
+        before = f(x,y)
+        learn(x,y,target)
+        after = f(x,y)
+        print 'Example (', x, ',', y, ',', target, '):'
+        print '    f before learning: ', before, 
+        print '    f after learning : ', after
+    
+def targetFunction(x,y):
+    return sin(x-3.0)*cos(y) + normal(0,0.1)
+
+#Is given the number of sample sets to generate. 
+#Generates a new true z, and calls learn with it
+def train(numSteps):
+    for i in range(numSteps):
+        x = random() * 6.0
+        y = random() * 6.0
+        target = targetFunction(x,y)
+        learn(x,y,target)
+    
+def writeF(filename):
+    fout = open(filename, 'w')
+    steps = 50
+    for i in range(steps):
+        for j in range(steps):
+            target = f(i * 6.0/steps, j * 6.0/steps)
+            fout.write(repr(target) + ' ')
+        fout.write('\n')
+    fout.close()
+
+# Uses sampleSize iterations random values of x and y, to compare
+# the value of our estimates
+def MSE(sampleSize):
+    totalSE = 0.0
+    for i in range(sampleSize):
+        x = random() * 6.0
+        y = random() * 6.0
+        error = targetFunction(x,y) - f(x,y)
+        totalSE = totalSE + error * error
+    print 'The estimated MSE: ', (totalSE / sampleSize)
+    
+def test2():
+    train(20)
+    writeF('f20')
+    MSE(10000)
+    for i in range(10):
+        train(1000)
+        MSE(10000)
+    writeF('f10000')
+
+test2()
+
